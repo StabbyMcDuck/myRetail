7f3fefb690e37d5a97a91eea077c64244417b15b
Imhoff, Regina L <regina.imhoff@merck.com>
2019-09-15 14:53:03 -0500

    fixed controller tests

--- spec/requests/product_prices_spec.rb
+++ spec/requests/product_prices_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "ProductPrices", type: :request do
-  describe "GET /product_prices" do
-    it "works! (now write some real specs)" do
-      get product_prices_path
-      expect(response).to have_http_status(200)
-    end
-  end
+
 end
